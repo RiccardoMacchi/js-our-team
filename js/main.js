b27c8cf5d542f811cbf00e9e01d596845d6e75d5
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-18 14:48:22 +0200

    aggiunta files aggiunto arrey di oggetti

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Creazione arrey oggetti
+
+const membTeam = [
+    membro1 = {
+        "name" : "Wayne Barnett",
+        "role" : "Founder & CEO",
+        "image" : "wayne-barnett-founder-ceo.jpg"
+    },
+    membro2 = {
+        "name" : "Angela Caroll",
+        "role" : "Chief Editor",
+        "image" : "angela-caroll-chief-editor.jpg"
+    },
+    membro3 = {
+        "name" : "Walter Gordon",
+        "role" : "Office Manager",
+        "image" : "walter-gordon-office-manager.jpg"
+    },
+    membro4 = {
+        "name" : "Angela Lopez",
+        "role" : "Social Media Manager",
+        "image" : "angela-lopez-social-media-manager.jpg"
+    },
+    membro5 = {
+        "name" : "Scott Estrada",
+        "role" : "Developer",
+        "image" : "scott-estrada-developer.jpg"
+    },
+    membro6 = {
+        "name" : "Barbara Ramos",
+        "role" : "Graphic Designer",
+        "image" : "barbara-ramos-graphic-designer.jpg"
+    },
+    
+]
+
+console.log(membTeam)
